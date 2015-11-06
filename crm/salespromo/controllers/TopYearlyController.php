633470c15ae69cef408f533dbb668024cf6df2b5
Piter Novian <piter@lipat.co.id>
2015-11-06 20:41:09 +0700

    sales

---
+++ crm/salespromo/controllers/TopYearlyController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace crm\salespromo\controllers;
+
+use yii\web\Controller;
+
+class TopYearlyController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
---
+++ crm/salespromo/views/sales-daily/index.php
@@ -0,0 +1,16 @@
+<?php
+$this->title = 'Crm-LukisonGroup';
+?>
+
+<div class="content">
+	<div class="content">					
+		 <div class="row">
+			<div class="col-md-12">						
+				<h4 class="page-head-line">Entry jumlah Penjualan Harian</h4>
+				<p>
+					Masukan Jumlah penjualan Per-Item Barang dalam satu hari, untuk semua customer moderen market dan traditional market.
+				</p>
+			</div>
+		</div>				 	
+	</div>				
+</div>
