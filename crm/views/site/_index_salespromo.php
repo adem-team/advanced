0dbf510a63d48fe28bf7a45cb0c1c0f361615ce9
ptrnov <ptr.nov@gmail.com>
2015-11-07 17:48:50 +0700

    rev

--- crm/views/site/_index_salespromo.php
+++ crm/views/site/_index_salespromo.php
@@ -33,6 +33,8 @@ $EmpDashboard=$this->render('login_index/_dashboard', [
 ]);
 ?>
 
+
+
 <div class="container-fluid" style="padding-left: 20px; padding-right: 20px" >
 		<div class="row">
 					<?php
@@ -53,21 +55,33 @@ $EmpDashboard=$this->render('login_index/_dashboard', [
 							'heading' => 'Data Prosess',
 							'postBody' => Html::listGroup([
 									[
+										/* 
+										 * Modul Sales Promotion - Stock Gudang | input data stock gudang di customer
+										*/
 										'content' => 'Stock Gudang',
 										'url' => '/salespromo/stock-gudang',
 										'badge' => '0'
 									],
 									[
+										/* 
+										 * Modul Sales Promotion - Stock Gudang | input data stock promo di customer
+										*/
 										'content' => 'Stock Promo',
 										'url' => '/salespromo/stock-promo',
 										'badge' => '0'
 									],
 									[
+										/* 
+										 * Modul Sales Promotion - Stock Penjualan | input Penjualan harian di Customer
+										*/
 										'content' => 'Penjualan',
 										'url' => '/salespromo/penjualan',
 										'badge' => '0'
 									],
 									[
+										/* 
+										 * Modul Sales Promotion - Stock Penjualan | input penualan Promo di Customer
+										*/
 										'content' => 'Promo',
 										'url' => '/salespromo/penjualan-promo',
 										'badge' => '0'
@@ -86,22 +100,34 @@ $EmpDashboard=$this->render('login_index/_dashboard', [
 							'heading' => 'Widget',
 							'postBody' => Html::listGroup([
 									[
-										'content' => 'Jadwak Kunjungan',
+										/* 
+										 * Modul Sales Promotion | report Jadwal kunjungan yang di berikan oleh salesmen untuk kunjungan ke customer per-minggu/bulan
+										*/
+										'content' => 'Jadwal Kunjungan',
 										'url' => '/salespromo/schadule',
 										'badge' => '0'
 									],
 									[
+										/* 
+										 * Modul Sales Promotion | Peta posisi alamat customer yang akan di kunjungi, check validasi kunjungan Porsquare
+										*/
 										'content' => 'Peta Customer',
 										'url' => '/salespromo/customer-map',
 										'badge' => '0'
 									],
 									[
-										'content' => 'Abseni',
+										/* 
+										 * Modul Sales Promotion | Salespromo check absensi pribadi | Bonus | Fasilitas
+										*/
+										'content' => 'Absensi',
 										'url' => '/salespromo/absensi',
 										'badge' => '0'
 									],
 									[
-										'content' => 'Profile',
+										/* 
+										 * Modul Sales Promotion | Salespromo Profile Pribadi | change password Account
+										*/
+										'content' => 'Profile',	
 										'url' => '/salespromo/profile',
 										'badge' => '0'
 									],
@@ -118,21 +144,33 @@ $EmpDashboard=$this->render('login_index/_dashboard', [
 							'heading' => 'Reporting Sales',
 							'postBody' => Html::listGroup([
 									[
+										/* 
+										 * Modul Sales Promotion | Reporting Prodak yang akan di jual oleh sales promotion
+										*/
 										'content' => 'Prodak',
 										'url' => '/salespromo/prodak',
 										'badge' => '0'
 									],	
 									[
-										'content' => 'Penjualan perHarian',
+										/* 
+										 * Modul Sales Promotion | Reporting pejualan harian semua sales promotion [Topdaily|BottemDaily]
+										*/
+										'content' => 'Penjualan Harian',
 										'url' => '/salespromo/report-daily',
 										'badge' => '0'
 									],									
 									[
-										'content' => 'Penjualan perBulanan',
+										/* 
+										 * Modul Sales Promotion | Reporting pejualan bulanan semua sales promotion [Topdaily|BottemDaily]
+										*/
+										'content' => 'Penjualan Bulanan',
 										'url' => '/salespromo/report-monthly',
 										'badge' => '0'
 									],
 									[
+										/* 
+										 * Modul Sales Promotion | Reporting pejualan tahunan semua sales promotion [Topdaily|BottemDaily]
+										*/
 										'content' => 'Penjualan perTahun',
 										'url' => '/salespromo/report-monthly',
 										'badge' => '0'
--- crm/views/site/index.php
+++ crm/views/site/index.php
@@ -1,3 +1,17 @@
 <?php
-include('index_salespromo.php');
+/*
+ * Login Dashboard user Permission
+ * login user account berdasarkan POSITION_LOGIN
+ * 0 = Salesman
+ * 1 = SPG
+ * 2 = Customer
+ * 3 = Distributor
+ * 4 = Factory/Pabrik
+ * Logic if (POSITION_LOGIN==1){}
+*/
+
+//include('_index_salespromo.php');
+//include('_index_salesman.php');
+include('_index_customer.php');
+
 ?>
