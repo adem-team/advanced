d61d27c7f9025c631070dbb859851f9762efdad7
devan <ivan_darmansius@yahoo.com>
2015-11-12 18:26:17 +0700

    rev1

---
+++ lukisongroup/!!master/views/customer/_search.php
@@ -0,0 +1,61 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model lukisongroup\models\master\CustomerSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="customer-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'CUST_KD') ?>
+
+    <?= $form->field($model, 'CUST_NM') ?>
+
+    <?= $form->field($model, 'ALAMAT') ?>
+
+    <?= $form->field($model, 'PIC') ?>
+
+    <?= $form->field($model, 'TLP1') ?>
+
+    <?php // echo $form->field($model, 'TLP2') ?>
+
+    <?php // echo $form->field($model, 'FAX') ?>
+
+    <?php // echo $form->field($model, 'EMAIL') ?>
+
+    <?php // echo $form->field($model, 'WEBSITE') ?>
+
+    <?php // echo $form->field($model, 'NOTE') ?>
+
+    <?php // echo $form->field($model, 'STATUS') ?>
+
+    <?php // echo $form->field($model, 'NPWP') ?>
+
+    <?php // echo $form->field($model, 'STT_TOKO') ?>
+
+    <?php // echo $form->field($model, 'CREATED_BY') ?>
+
+    <?php // echo $form->field($model, 'CREATED_AT') ?>
+
+    <?php // echo $form->field($model, 'UPDATED_AT') ?>
+
+    <?php // echo $form->field($model, 'UPDATED_BY') ?>
+
+    <?php // echo $form->field($model, 'DATA_ALL') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
