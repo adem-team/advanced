78df5880603d8a5e426de7c3415bd9b2faebf5e8
ptrnov <ptr.nov@gmail.com>
2016-01-19 10:08:54 +0700

    wawan spl

--- lukisongroup/master/views/suplier/_update.php
+++ lukisongroup/master/views/suplier/_update.php
@@ -26,6 +26,8 @@ use lukisongroup\hrd\models\Corp;
     
     <?= $form->field($model, 'NM_SUPPLIER')->textInput(['maxlength' => true]) ?>
 
+    <?= $form->field($model, 'PIC')->textInput(['maxlength' => true]) ?>
+
     <?= $form->field($model, 'ALAMAT')->textarea(['rows' => 6]) ?>
 
     <?= $form->field($model, 'KOTA')->textInput(['maxlength' => true]) ?>
