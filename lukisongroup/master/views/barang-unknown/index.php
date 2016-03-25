2ea8819f5750bfac4ceb9e1e3f368fb08e24fb99
ptrnov <ptr.nov@gmail.com>
2016-03-26 01:42:55 +0700

    server

--- lukisongroup/master/views/barang-unknown/index.php
+++ lukisongroup/master/views/barang-unknown/index.php
@@ -278,6 +278,7 @@ $this->title = Yii::t('app', 'Umum - Barang ');
 				'dropdown' => true,
 				'template' => '{view}{update}{price}',
 				'dropdownOptions'=>['class'=>'pull-right dropup'],
+				'dropdownButton'=>['class'=>'btn btn-default btn-xs'],
 				'buttons' => [
 						'view' =>function($url, $model, $key){
 								return  '<li>' .Html::a('<span class="fa fa-eye fa-dm"></span>'.Yii::t('app', 'View'),
