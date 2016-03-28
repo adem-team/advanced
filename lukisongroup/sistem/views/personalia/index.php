69075e7acdf46e17c809c4665cf51ea7049caf04
ptrnov <ptr.nov@gmail.com>
2016-03-28 11:54:24 +0700

    wawan

--- lukisongroup/sistem/views/personalia/index.php
+++ lukisongroup/sistem/views/personalia/index.php
@@ -35,7 +35,7 @@ $JSCode = <<<EOF
 
 function(start, end) {
 	var dateTime2 = new Date(end);
-	var dateTime1 = new Date(start);	
+	var dateTime1 = new Date(start);
 	tgl1 = moment(dateTime1).format("YYYY-MM-DD HH:mm:ss");
 	tgl2 = moment(dateTime2).format("YYYY-MM-DD HH:mm:ss");
 
@@ -58,9 +58,9 @@ function(calEvent, jsEvent, view) {
 	 $('#modalTitle').html(calEvent.title);
 	$('#modalBody').html(calEvent.id);
     $('#confirm-permission-alert').modal();
-	
-	
-	
+
+
+
 	//$.fn.modal.Constructor.prototype.enforceFocus = function() {};
 	// $('#confirm-permission-alert').on('show.bs.modal', function (event) {
 		// var button = $(event.relatedTarget)
@@ -72,82 +72,82 @@ function(calEvent, jsEvent, view) {
 		// $.post(href)
 			// .done(function( data ) {
 				// modal.find('.modal-body').html(data)
-			// }); 
+			// });
 		// }),
 }
 EOF;
 
 	/**
-     * Setting 
+     * Setting
 	 * @author ptrnov  <piter@lukison.com>
 	 * @since 1.1
      */
-	function tombolSetting(){		
+	function tombolSetting(){
 		$title1 = Yii::t('app', 'Setting');
-		$options1 = [ 'id'=>'setting',	
+		$options1 = [ 'id'=>'setting',
 					  //'data-toggle'=>"modal",
-					  'data-target'=>"#profile-setting",											
+					  'data-target'=>"#profile-setting",
 					  //'class' => 'btn btn-default',
 					  'style' => 'text-align:left',
-		]; 
+		];
 		$icon1 = '<span class="fa fa-cogs fa-md"></span>';
 		$label1 = $icon1 . ' ' . $title1;
 		$url1 = Url::toRoute(['/sistem/user-profile/setting']);//,'kd'=>$kd]);
 		$content = Html::a($label1,$url1, $options1);
 		return $content;
 	}
-	
+
 	/**
      * New|Change|Reset| Password Login
 	 * @author ptrnov  <piter@lukison.com>
 	 * @since 1.1
      */
-	function tombolPasswordUtama(){		
+	function tombolPasswordUtama(){
 		$title1 = Yii::t('app', 'Password');
-		$options1 = [ 'id'=>'password',	
+		$options1 = [ 'id'=>'password',
 					  'data-toggle'=>"modal",
-					  'data-target'=>"#profile-password",											
+					  'data-target'=>"#profile-password",
 					  //'class' => 'btn btn-default',
 					 // 'style' => 'text-align:left',
-		]; 
+		];
 		$icon1 = '<span class="fa fa-shield fa-md"></span>';
 		$label1 = $icon1 . ' ' . $title1;
 		$url1 = Url::toRoute(['/sistem/user-profile/password-utama-view']);
 		$content = Html::a($label1,$url1, $options1);
 		return $content;
 	}
-	
+
 	/**
-     * Create Signature 
+     * Create Signature
 	 * @author ptrnov  <piter@lukison.com>
 	 * @since 1.1
      */
-	function tombolSignature(){		
+	function tombolSignature(){
 		$title1 = Yii::t('app', 'Signature');
-		$options1 = [ 'id'=>'signature',	
+		$options1 = [ 'id'=>'signature',
 					  //'data-toggle'=>"modal",
-					  'data-target'=>"#profile-signature",											
+					  'data-target'=>"#profile-signature",
 					  //'class' => 'btn btn-default',
-		]; 
+		];
 		$icon1 = '<span class="fa fa-pencil-square-o fa-md"></span>';
 		$label1 = $icon1 . ' ' . $title1;
 		$url1 = Url::toRoute(['/sistem/user-profile/signature']);//,'kd'=>$kd]);
 		$content = Html::a($label1,$url1, $options1);
 		return $content;
 	}
-	
+
 	/**
      * Persinalia Employee
 	 * @author ptrnov  <piter@lukison.com>
 	 * @since 1.1
      */
-	function tombolPersonalia(){		
+	function tombolPersonalia(){
 		$title1 = Yii::t('app', 'My Personalia');
-		$options1 = [ 'id'=>'personalia',	
+		$options1 = [ 'id'=>'personalia',
 					  //'data-toggle'=>"modal",
-					  'data-target'=>"#profile-personalia",											
+					  'data-target'=>"#profile-personalia",
 					  'class' => 'btn btn-primary',
-		]; 
+		];
 		$icon1 = '<span class="fa fa-group fa-md"></span>';
 		$label1 = $icon1 . ' ' . $title1;
 		$url1 = Url::toRoute(['/sistem/personalia']);//,'kd'=>$kd]);
@@ -160,13 +160,13 @@ EOF;
 	 * @author ptrnov  <piter@lukison.com>
 	 * @since 1.1
      */
-	function tombolPerformance(){		
+	function tombolPerformance(){
 		$title1 = Yii::t('app', 'My Performance');
-		$options1 = [ 'id'=>'performance',	
+		$options1 = [ 'id'=>'performance',
 					  //'data-toggle'=>"modal",
-					  'data-target'=>"#profile-performance",											
+					  'data-target'=>"#profile-performance",
 					  'class' => 'btn btn-danger',
-		]; 
+		];
 		$icon1 = '<span class="fa fa-graduation-cap fa-md"></span>';
 		$label1 = $icon1 . ' ' . $title1;
 		$url1 = Url::toRoute(['/sistem/performance']);//,'kd'=>$kd]);
@@ -178,13 +178,13 @@ EOF;
 	 * @author ptrnov  <piter@lukison.com>
 	 * @since 1.1
      */
-	function tombolLogoff(){		
+	function tombolLogoff(){
 		$title1 = Yii::t('app', 'Logout');
-		$options1 = [ 'id'=>'logout',	
+		$options1 = [ 'id'=>'logout',
 					  //'data-toggle'=>"modal",
-					  'data-target'=>"#profile-logout",											
+					  'data-target'=>"#profile-logout",
 					  //'class' => 'btn btn-default',
-		]; 
+		];
 		$icon1 = '<span class="fa fa-power-off fa-lg"></span>';
 		$label1 = $icon1 . ' ' . $title1;
 		$url1 = Url::toRoute(['/sistem/user-profile/logoff']);//,'kd'=>$kd]);
@@ -204,7 +204,7 @@ EOF;
 	$hdrLabel1=[];
 	//$hdrLabel2=[];
 	$getHeaderLabelWrap=[];
-	
+
 	/*
 	 * Terminal ID | Mashine
 	 * Colomn 1
@@ -274,7 +274,7 @@ EOF;
 			 ]
 		 ],
 	];
-	
+
 	/*
 	 * Employe name
 	 * Colomn 2
@@ -323,8 +323,8 @@ EOF;
 		],
 		//'footer'=>true,
 	];
-	
-	
+
+
 	foreach($dataProviderField as $key =>$value)
 	{
 		$i=2;
@@ -469,24 +469,24 @@ EOF;
 					],
 					//'ajaxEvents' => Url::toRoute(['/site/jsoncalendar'])
 				]);
-				
+
 				echo Html::panel(
 					['heading' => 'CLENDER', 'body' =>$calenderRt],
 					Html::TYPE_DANGER
-				);				
-				
+				);
+
 				/*
-				 * LIST EVENT CALENDAR 
+				 * LIST EVENT CALENDAR
 				 * PERIODE 23-22
 				 * @author ptrnov  [piter@lukison.com]
 				 * @since 1.2
 				*/
 				$actionClass='btn btn-info btn-xs';
 				$actionLabel='Update';
-				$attDinamikEvent =[];				
+				$attDinamikEvent =[];
 				/*GRIDVIEW ARRAY FIELD HEAD*/
 				$headColomnEvent=[
-					['ID' =>0, 'ATTR' =>['FIELD'=>'start','SIZE' => '10px','label'=>'DATE START','align'=>'left','warna'=>'97, 211, 96, 0.3']],				
+					['ID' =>0, 'ATTR' =>['FIELD'=>'start','SIZE' => '10px','label'=>'DATE START','align'=>'left','warna'=>'97, 211, 96, 0.3']],
 					['ID' =>1, 'ATTR' =>['FIELD'=>'end','SIZE' => '10px','label'=>'DATE END','align'=>'left','warna'=>'97, 211, 96, 0.3']],
 					['ID' =>2, 'ATTR' =>['FIELD'=>'title','SIZE' => '10px','label'=>'TITLE','align'=>'left','warna'=>'97, 211, 96, 0.3']],
 				];
@@ -510,7 +510,7 @@ EOF;
 															'data-toggle'=>"modal",
 															'data-target'=>"#alias-cust",
 															]). '</li>' . PHP_EOL;
-						},				
+						},
 						'review' =>function($url, $model, $key){
 								return  '<li>' . Html::a('<span class="fa fa-retweet fa-dm"></span>'.Yii::t('app', 'Set Alias Prodak'),
 															['/sistem/personalia/view','id'=>$model->id],[
@@ -518,14 +518,14 @@ EOF;
 															'data-toggle'=>"modal",
 															'data-target'=>"#alias-prodak",
 															]). '</li>' . PHP_EOL;
-						},	
+						},
 						'delete' =>function($url, $model, $key){
 								return  '<li>' . Html::a('<span class="fa fa-retweet fa-dm"></span>'.Yii::t('app', 'new Customer'),
 															['/sistem/personalia/view','id'=>$model->id],[
 															'data-toggle'=>"modal",
 															'data-target'=>"#alias-prodak",
 															]). '</li>' . PHP_EOL;
-						},				
+						},
 					],
 					'headerOptions'=>[
 						'style'=>[
@@ -548,16 +548,16 @@ EOF;
 				];
 				/*GRIDVIEW ARRAY ROWS*/
 				foreach($gvHeadColomnEvent as $key =>$value[]){
-					$attDinamikEvent[]=[		
+					$attDinamikEvent[]=[
 						'attribute'=>$value[$key]['FIELD'],
 						'label'=>$value[$key]['label'],
 						'filter'=>true,
 						'hAlign'=>'right',
 						'vAlign'=>'middle',
 						//'mergeHeader'=>true,
-						'noWrap'=>true,			
-						'headerOptions'=>[		
-								'style'=>[									
+						'noWrap'=>true,
+						'headerOptions'=>[
+								'style'=>[
 								'text-align'=>'center',
 								'width'=>$value[$key]['FIELD'],
 								'font-family'=>'tahoma, arial, sans-serif',
@@ -565,7 +565,7 @@ EOF;
 								//'background-color'=>'rgba(97, 211, 96, 0.3)',
 								'background-color'=>'rgba('.$value[$key]['warna'].')',
 							]
-						],  
+						],
 						'contentOptions'=>[
 							'style'=>[
 								'text-align'=>$value[$key]['align'],
@@ -578,17 +578,17 @@ EOF;
 						//'pageSummary'=>true,
 						// 'pageSummaryOptions' => [
 							// 'style'=>[
-									// 'text-align'=>'right',		
+									// 'text-align'=>'right',
 									//'width'=>'12px',
 									// 'font-family'=>'tahoma',
-									// 'font-size'=>'8pt',	
+									// 'font-size'=>'8pt',
 									// 'text-decoration'=>'underline',
 									// 'font-weight'=>'bold',
-									// 'border-left-color'=>'transparant',		
-									// 'border-left'=>'0px',									
+									// 'border-left-color'=>'transparant',
+									// 'border-left'=>'0px',
 							// ]
-						// ],	
-					];	
+						// ],
+					];
 				};
 				/*SHOW GRID VIEW LIST EVENT*/
 				echo GridView::widget([
@@ -623,7 +623,7 @@ EOF;
 					'responsiveWrap'=>true,
 					'bordered'=>true,
 					'striped'=>true,
-				]); 				
+				]);
 			?>
 		</div>
 		<div class="col-sm-8 col-md-8 col-lg-8" >
@@ -631,11 +631,11 @@ EOF;
 				echo Yii::$app->controller->renderPartial('_head_chart',[
 							//'model_CustPrn'=>$model_CustPrn,
 							//'count_CustPrn'=>$count_CustPrn
-				]);			
+				]);
 			?>
-			<?php 
+			<?php
 				/*
-				 * DAILY LOG PERSONAL ABSENSI 
+				 * DAILY LOG PERSONAL ABSENSI
 				 * PERIODE 23-22
 				 * @author ptrnov  [piter@lukison.com]
 				 * @since 1.2
@@ -679,7 +679,7 @@ EOF;
 					//'export' => false,
 				]);
 			?>
-		</div>	
+		</div>
 		<div class="col-sm-8 col-md-8 col-lg-8" >
 			<div  class="row" style="margin-left:5px">
 					<!-- IJIN !-->
@@ -687,8 +687,8 @@ EOF;
 						echo Yii::$app->controller->renderPartial('_button_ijin',[
 								//'model_CustPrn'=>$model_CustPrn,
 								//'count_CustPrn'=>$count_CustPrn
-						]);		
-					?>					
+						]);
+					?>
 					<!-- CUTI !-->
 					<div class="btn-group pull-left">
 						<button type="button" class="btn btn-info">CUTI</button>
