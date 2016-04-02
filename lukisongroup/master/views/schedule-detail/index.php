67e16c0b6d749f5cb08ecaba9f8a10340c5b3dc7
ptrnov <ptr.nov@gmail.com>
2016-04-02 23:25:03 +0700

    server

--- lukisongroup/master/views/schedule-detail/index.php
+++ lukisongroup/master/views/schedule-detail/index.php
@@ -1,47 +1,168 @@
 <?php
-
 use yii\helpers\Html;
-use yii\grid\GridView;
+use kartik\grid\GridView;
+use yii\helpers\Url;
+use yii\widgets\Pjax;
+use yii\helpers\ArrayHelper;
+use yii\bootstrap\Modal;
+use yii\helpers\Json;
+use yii\web\Request;
+use kartik\daterange\DateRangePicker;;
 
-/* @var $this yii\web\View */
-/* @var $searchModel lukisongroup\master\models\ScheduledetailSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
-
-$this->title = 'Scheduledetails';
-$this->params['breadcrumbs'][] = $this->title;
+$this->sideCorp = 'PT.Effembi Sukses Makmur';                          /* Title Select Company pada header pasa sidemenu/menu samping kiri */
+$this->sideMenu = 'esm_customers';                                  /* kd_menu untuk list menu pada sidemenu, get from table of database */
+$this->title = Yii::t('app', 'ESM - Group');          /* title pada header page */
+$this->params['breadcrumbs'][] = $this->title;                      /* belum di gunakan karena sudah ada list sidemenu, on plan next*/
 ?>
-<div class="scheduledetail-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <?= Html::a('Create Scheduledetail', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'ID',
-            'TGL',
-            'CUST_ID',
-            'USER_ID',
-            'SCDL_GROUP',
-            // 'LAT',
-            // 'LAG',
-            // 'RADIUS',
-            // 'NOTE:ntext',
-            // 'STATUS',
-            // 'CREATE_BY',
-            // 'CREATE_AT',
-            // 'UPDATE_BY',
-            // 'UPDATE_AT',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-
+<div class="container-fluid" style="font-family: verdana, arial, sans-serif ;font-size: 8pt;">
+    <div  class="row" style="margin-top:15px">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+			<?php
+				/*
+				 * DAILY CUSTOMERS VISIT
+				 * @author ptrnov  [piter@lukison.com]
+				 * @since 1.2
+				*/
+				$actionClass='btn btn-info btn-xs';
+				$actionLabel='View';
+				$attDinamik =[];
+				/*GRIDVIEW ARRAY FIELD HEAD*/
+				$headColomnEvent=[
+					['ID' =>0, 'ATTR' =>['FIELD'=>'TGL','SIZE' => '10px','label'=>'CORP','align'=>'left','warna'=>'249, 215, 100, 1','GRP'=>false,'FORMAT'=>'html','filter'=>$aryCorpID,'filterType'=>GridView::FILTER_SELECT2,'filterwarna'=>'249, 215, 100, 1']],
+					['ID' =>1, 'ATTR' =>['FIELD'=>'USER_ID','SIZE' => '10px','label'=>'DATE','align'=>'left','warna'=>'249, 215, 100, 1','GRP'=>false,'FORMAT'=>'html','filter'=>true,'filterType'=>false,'filterwarna'=>'249, 215, 100, 1']],
+					['ID' =>2, 'ATTR' =>['FIELD'=>'CUST_ID','SIZE' => '10px','label'=>'KD.PO','align'=>'left','warna'=>'249, 215, 100, 1','GRP'=>false,'FORMAT'=>'html','filter'=>true,'filterType'=>false,'filterwarna'=>'249, 215, 100, 1']],
+					['ID' =>3, 'ATTR' =>['FIELD'=>'SCDL_GROUP','SIZE' => '10px','label'=>'COSTCENTER','align'=>'left','warna'=>'249, 215, 100, 1','GRP'=>false,'FORMAT'=>'html','filter'=>true,'filterType'=>false,'filterwarna'=>'249, 215, 100, 1']],
+				];
+				$gvHeadColomn = ArrayHelper::map($headColomnEvent, 'ID', 'ATTR');
+				
+				/*GRIDVIEW ARRAY ACTION*/
+				$attDinamik[]=[
+					'class'=>'kartik\grid\ActionColumn',
+					'dropdown' => true,
+					'template' => '{view}',
+					'dropdownOptions'=>['class'=>'pull-left dropdown','style'=>['disable'=>true]],
+					'dropdownButton'=>['class'=>'btn btn-default btn-xs'],
+					'dropdownButton'=>[
+						'class' => $actionClass,
+						'label'=>$actionLabel,
+						//'caret'=>'<span class="caret"></span>',
+					],
+					'buttons' => [
+						'view' =>function($url, $model, $key){
+								return  '<li>' .Html::a('<span class="fa fa-search-plus fa-dm"></span>'.Yii::t('app', 'View PO'),
+															['/sistem/personalia/view','id'=>$model->ID],[
+															'id'=>'alias-cust-id',
+															'data-toggle'=>"modal",
+															'data-target'=>"#alias-cust",
+															]). '</li>' . PHP_EOL;
+						},
+					],
+					'headerOptions'=>[
+						'style'=>[
+							'text-align'=>'center',
+							'width'=>'10px',
+							'font-family'=>'tahoma, arial, sans-serif',
+							'font-size'=>'9pt',
+							'background-color'=>'rgba(74, 206, 231, 1)',
+						]
+					],
+					'contentOptions'=>[
+						'style'=>[
+							'text-align'=>'center',
+							'width'=>'10px',
+							'height'=>'10px',
+							'font-family'=>'tahoma, arial, sans-serif',
+							'font-size'=>'9pt',
+						]
+					],
+				];
+										
+				/*GRIDVIEW ARRAY ROWS*/
+				foreach($gvHeadColomn as $key =>$value[]){
+					$attDinamik[]=[
+						'attribute'=>$value[$key]['FIELD'],
+						'label'=>$value[$key]['label'],
+						'filterType'=>$value[$key]['filterType'],
+						'filter'=>$value[$key]['filter'],
+						'filterOptions'=>['style'=>'background-color:rgba('.$value[$key]['filterwarna'].'); align:center'],
+						'hAlign'=>'right',
+						'vAlign'=>'middle',
+						//'mergeHeader'=>true,
+						'noWrap'=>true,
+						'group'=>$value[$key]['GRP'],
+						'format'=>$value[$key]['FORMAT'],						
+						'headerOptions'=>[
+								'style'=>[
+								'text-align'=>'center',
+								'width'=>$value[$key]['FIELD'],
+								'font-family'=>'tahoma, arial, sans-serif',
+								'font-size'=>'8pt',
+								//'background-color'=>'rgba(74, 206, 231, 1)',
+								'background-color'=>'rgba('.$value[$key]['warna'].')',
+							]
+						],
+						'contentOptions'=>[
+							'style'=>[
+								'text-align'=>$value[$key]['align'],
+								'font-family'=>'tahoma, arial, sans-serif',
+								'font-size'=>'8pt',
+								//'background-color'=>'rgba(13, 127, 3, 0.1)',
+							]
+						],
+						//'pageSummaryFunc'=>GridView::F_SUM,
+						//'pageSummary'=>true,
+						// 'pageSummaryOptions' => [
+							// 'style'=>[
+									// 'text-align'=>'right',
+									//'width'=>'12px',
+									// 'font-family'=>'tahoma',
+									// 'font-size'=>'8pt',
+									// 'text-decoration'=>'underline',
+									// 'font-weight'=>'bold',
+									// 'border-left-color'=>'transparant',
+									// 'border-left'=>'0px',
+							// ]
+						// ],
+					];
+				};
+				
+				/*SHOW GRID VIEW LIST EVENT*/
+				echo GridView::widget([
+					'id'=>'cust-visit-list',
+					'dataProvider' => $dataProvider,
+					'filterModel' => $searchModel,					
+					//'filterRowOptions'=>['style'=>'background-color:rgba(74, 206, 231, 1); align:center'],
+					'columns' => $attDinamik,
+					/* [
+						['class' => 'yii\grid\SerialColumn'],
+						'start',
+						'end',
+						'title',
+						['class' => 'yii\grid\ActionColumn'],
+					], */
+					'pjax'=>true,
+					'pjaxSettings'=>[
+						'options'=>[
+							'enablePushState'=>false,
+							'id'=>'cust-visit-list',
+						],
+					],
+					'panel' => [
+								'heading'=>'<h3 class="panel-title">DAILY CUSTOMERS VISIT</h3>',
+								'type'=>'info',
+								//'showFooter'=>false,
+					],
+					'toolbar'=> [
+						''//'{items}',
+					],
+					'hover'=>true, //cursor select
+					'responsive'=>true,
+					'responsiveWrap'=>true,
+					'bordered'=>true,
+					'striped'=>true,
+				]);
+			?>
+		</div>
+	</div>
 </div>
