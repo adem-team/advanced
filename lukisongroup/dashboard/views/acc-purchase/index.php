188b9319afb66732e7b922adacd5ea85f2c4ab43
ptrnov <piter@lipat.co.id>
2016-04-09 12:55:47 +0700

    server

--- lukisongroup/dashboard/views/acc-purchase/index.php
+++ lukisongroup/dashboard/views/acc-purchase/index.php
@@ -8,61 +8,53 @@ use kartik\widgets\ActiveForm;
 use kartik\tabs\TabsX;
 use kartik\date\DatePicker;
 use kartik\builder\Form;
-use scotthuangzl\googlechart\GoogleChart;
-
-//use backend\assets\AppAsset; 	/* CLASS ASSET CSS/JS/THEME Author: -ptr.nov-*/
-//AppAsset::register($this);		/* INDEPENDENT CSS/JS/THEME FOR PAGE  Author: -ptr.nov-*/
+use yii\helpers\Json;
+use yii\web\Response;
+use lukisongroup\assets\AppAssetFusionChart;
+AppAssetFusionChart::register($this);
 
 $this->sideCorp = 'PT.Lukisongroup';                          /* Title Select Company pada header pasa sidemenu/menu samping kiri */
 $this->sideMenu = 'acc_purchase';                                      /* kd_menu untuk list menu pada sidemenu, get from table of database */
 $this->title = Yii::t('app', 'Accounting');              /* title pada header page */
 $this->params['breadcrumbs'][] = $this->title;                      /* belum di gunakan karena sudah ada list sidemenu, on plan next*/
+				
+?>
 
-				$pertama= GoogleChart::widget(array('visualization' => 'PieChart',
-					'data' => array(
-						array('Task', 'Hours per Day'),
-						array('Work', 11),
-						array('Eat', 2),
-						array('Commute', 2),
-						array('Watch TV', 2),
-						array('Sleep', 7)
-					),
-				//'options' => array('title' => 'Employee Properties')
-                ));
+<?php
+	
+	
+	echo Html::panel(
+		['heading' => 'Employee Properties', 'body' =>$kedua],
+		Html::TYPE_SUCCESS
+	);
 
-
-				$kedua= GoogleChart::widget(array('visualization' => 'PieChart',
-					'data' => array(
-						array('Task', 'Hours per Day'),
-						array('Work', 11),
-						array('Eat', 2),
-						array('Commute', 2),
-						array('Watch TV', 2),
-						array('Sleep', 7)
-					),
-				//'options' => array('title' => 'Employee Status')
-                ));
-			?>
-
+?>
 
 <div class="body-content">
     <div class="row" style="padding-left: 5px; padding-right: 5px">
-
-        <div class="col-sm-4 col-md-4 col-lg-4 ">
-            <?php
-            echo Html::panel(
-                ['heading' => 'Employee Status', 'body' => $pertama],
-                Html::TYPE_SUCCESS
-            );
-            ?>
-        </div>
-        <div class="col-sm-4 col-md-4 col-lg-4">
-            <?php
-            echo Html::panel(
-                ['heading' => 'Employee Properties', 'body' =>$kedua],
-                Html::TYPE_SUCCESS
-            );
-            ?>
-        </div>
+        <div class="col-sm-6 col-md-6 col-lg-6 ">
+			<?php
+				$content_cc_support=Yii::$app->controller->renderPartial('graph_cc_support',[
+					// 'model_CustPrn'=>$model_CustPrn,
+					// 'count_CustPrn'=>$count_CustPrn
+				]);
+				echo Html::panel(
+					['heading' => 'Cost Center Support', 'body' => $content_cc_support],
+					Html::TYPE_INFO
+				);
+			?>
+		</div>
+		<div class="col-sm-6 col-md-6 col-lg-6 ">
+			<?php
+				$content_cc_bisnis=Yii::$app->controller->renderPartial('graph_cc_bisnis',[
+					// 'model_CustPrn'=>$model_CustPrn,
+					// 'count_CustPrn'=>$count_CustPrn
+				]);
+				echo Html::panel(
+					['heading' => 'Cost Center Businis', 'body' => $content_cc_bisnis],
+					Html::TYPE_INFO
+				);
+			?>
+		</div>
     </div>
 </div>
