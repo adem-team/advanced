9cc33b80518be2eb89598a02ddd9c7b2d045401e
ptrnov <ptr.nov@gmail.com>
2016-06-20 20:29:48 +0700

    server

--- crm/front/views/contact/index.php
+++ crm/front/views/contact/index.php
@@ -1,6 +1,6 @@
 <?php
 /* @var $this yii\web\View */
-
+use kartik\helpers\Html;
 $this->title = 'lukisongroup';
 ?>
 
@@ -11,12 +11,15 @@ $this->title = 'lukisongroup';
 				 <div class="row">
 					<div class="col-md-12">						
 						<h4 class="page-head-line">Contact Us</h4>
-                <p>Lukison group considered being one of the largest premium food court in the city and it is a great mix of old favorites and new restaurants. With the large space more than 2000m2 and futuristic and old style design, Lukison offered a coz/ambiance and warm place to enjoy your dinning experiences.
-
-complete with 3D lenticular light box and vintage wood plank will make the visitors feel relax and chill with new ambiance that they can’t discover in another place. With all variety of different kinds of food, Lukison give you many choices to eat and simplicity payment that will save your time
-
-And that’s where you come in. You’re an experienced professional with a passion for people and hospitality; someone who will proudly promote our brands while always striving to create the ultimate guest experience. At Prime you’ll enjoy competitive compensation and benefits, and advance your career with a team that values your contributions and helps you reach your goals.</p>
-
+						<?php
+							echo Html::well(Html::address(
+							'Ruko De Mansion Blok C No.12',
+							 ['Jl. Jalur Sutera, Alam Sutera, Serpong','Tangerang Selatan.'],
+							 ['Tel ' => '(021) 3044-85-98/99'],
+							 ['Fax ' => '(021) 3044 85 97'],
+							 ['Website : ' => 'www.lukison.com', 'Email' => 'info@lukison.com']
+							), Html::SIZE_TINY); 
+						?>	
 					</div>
 				</div>
 				 	
