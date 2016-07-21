2d9e5ec36f22726f43286564afe276e7adeac23c
ptrnov <ptr.nov@gmail.com>
2016-07-21 16:54:00 +0700

    server

---
+++ console/views/postman-customer/_postmanBody.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Lukisongroup Postman</title>
+	</head>
+	<body style="margin:0px;
+				padding:0px;
+				border: 0 none;
+				font-size: 11px;
+				font-family: Verdana, sans-serif;
+				background-color: #efefef;">
+		<table style="margin-button: 50px; width: 350px; margin: 50px auto 50px auto; border: 1px #fff solid; background: #fff; font-size: 12px; font-family: Verdana, sans-serif;" align="center">
+			<tbody>
+				<tr>
+					<td style="color: #fff; background:blue;text-align:center">
+						<h2>LUKISONGROUP POSTMAN</h2>
+					</td>
+				</tr>
+				<tr>
+					<td style="background: #ddd; margin-top:5px; margin-button: 18px; text-align: center;">
+						MASTER DATA CUSTOMER
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<table style="font-size: 12px; font-family: Verdana, sans-serif;">
+							<tr>
+								<td style="width: 90px; color:black; background:white;text-align:left">
+									TO
+								</td>
+								<td style="color:black; background:white;text-align:left">
+									:
+								</td>
+								<td style="color:black; background:white;text-align:left">
+									Sales & Marketing Department
+								</td>
+							</tr>
+							<tr>
+								<td style="color:black; background:white;text-align:left">
+									EVENT SEND
+								</td>
+								<td style="color:black; background:white;text-align:left">
+									:
+								</td>
+								<td style="color:black; background:white;text-align:left">
+									WEEKLY
+								</td>
+							</tr>
+							<tr>
+								<td style="color:black; background:white;text-align:left">
+									Total Customer
+								</td>
+								<td style="color:black; background:white;text-align:left">
+									:
+								</td>
+								<td style="color:black; background:white;text-align:left">
+									<?=$cusCount[0]['CNT_ALL']; ?>
+								</td>
+							</tr>
+							<tr>
+								<td style="color:black; background:white;text-align:left">
+									Created By
+								</td>
+								<td style="color:black; background:white;text-align:left">
+									:
+								</td>
+								<td style="color:black; background:white;text-align:left">
+									POSTMAN SYSTEM 
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+
+				<tr>
+					<td style="padding: 9px; color:red; background:white;text-align:center">
+						This email message has been automatically generated and sent every week, Please do not reply to this message.
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2" style="background: #ddd; padding: 9px; margin-top:5px; margin-button: 18px; text-align: center;">
+						&copy;Lukisongroup 2016
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</body>
+</html>
--- lukisongroup/programmer/controllers/FoursquareController.php
+++ lukisongroup/programmer/controllers/FoursquareController.php
@@ -13,6 +13,7 @@ use Yii;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter; 	
+use yii\data\ArrayDataProvider;
 /* VARIABLE PRIMARY JOIN/SEARCH/FILTER/SORT Author: -ptr.nov- */
 //use app\models\hrd\Dept;			/* TABLE CLASS JOIN */
 //use app\models\hrd\DeptSearch;		/* TABLE CLASS SEARCH */
@@ -48,7 +49,33 @@ class FoursquareController extends Controller
        // $searchModel_Dept = new DeptSearch();
 		//$dataProvider_Dept = $searchModel_Dept->search(Yii::$app->request->queryParams);
 		
-		return $this->render('index');
+		//return $this->render('index');
+		$tanggalOfMonth= new ArrayDataProvider([
+			//'key' => 'ID',
+			//SELECT a.USER_ID,a.SCDL_GROUP AS GRP_ID, b.SCDL_GROUP_NM as GRP_NM,a.CUST_ID,c.CUST_NM,a.LAT,a.LAG 
+			'allModels'=>Yii::$app->db_esm->createCommand("				
+				SELECT DATE_FORMAT( a.Date,'%Y-%m-%d') as TGL
+				FROM (
+						select LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 0 month)) - INTERVAL (a.a + (10 * b.a) + (100 * c.a)) DAY as Date
+						from (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as a
+						cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as b
+						cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as c
+				) a
+				WHERE a.Date between DATE_FORMAT((DATE_SUB(CURDATE(), INTERVAL DAYOFMONTH(CURRENT_DATE)-1 DAY)), '%Y-%m-01') and LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 0 month))
+				ORDER BY a.Date	
+			")->queryAll(),
+		]);			
+		$attributeField=$tanggalOfMonth->allModels;
+		//$attDinamik=[];
+		foreach($attributeField as $key =>$value){
+			$attDinamik[]= $value['TGL'];
+			//$attDinamik[]=$value[$key];
+		};
+		$attDinamik0=['nama','alamat'];
+		$jn=array_merge($attDinamik0,$attDinamik);
+		//print_r($attributeField);
+		print_r($jn);
     }
+	
 	
 }
