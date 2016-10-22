9a1aaec7c586f2c50c5882e8b3d9ea4d0cea7c02
Piter Novian <piter@lipat.co.id>
2016-10-22 13:10:12 +0700

    server

--- lukisongroup/widget/controllers/PilotprojectChatController.php
+++ lukisongroup/widget/controllers/PilotprojectChatController.php
@@ -47,7 +47,23 @@ class PilotprojectChatController extends Controller
 					'en',
 					'de',
 				],
-			]
+			],
+			/* 'corsFilter' => [
+				'class' => \yii\filters\Cors::className(),
+				'cors' => [
+						// restrict access to
+						'Origin' => ['localhost','http://lukisongroup.com', 'http://lukisongroup.int'],
+						'Access-Control-Request-Method' => ['POST', 'PUT','GET'],
+						// Allow only POST and PUT methods
+						'Access-Control-Request-Headers' => ['X-Wsse'],
+						// Allow only headers 'X-Wsse'
+						'Access-Control-Allow-Credentials' => true,
+						// Allow OPTIONS caching
+						'Access-Control-Max-Age' => 3600,
+						// Allow the X-Pagination-Current-Page header to be exposed to the browser.
+						'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
+					]		
+				], */
         ]);
 		
     }
@@ -55,13 +71,13 @@ class PilotprojectChatController extends Controller
 	/**
      * @inheritdoc
      */
-	public function actions()
+	/* public function actions()
 	{
 		$actions = parent::actions();
 		unset($actions['update'], $actions['create'], $actions['delete'], $actions['view']);
 		//unset($actions['update'], $actions['create'], $actions['delete'], $actions['view']);
 		return $actions;
-	}
+	} */
 		
 	public function actionIndex()
      {
