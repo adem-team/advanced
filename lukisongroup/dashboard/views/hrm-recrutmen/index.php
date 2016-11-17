9763483fb2965010143a8116bce98e47a81968d9
Piter Novian <piter@lipat.co.id>
2016-11-17 19:37:58 +0700

    server

--- lukisongroup/dashboard/views/hrm-recrutmen/index.php
+++ lukisongroup/dashboard/views/hrm-recrutmen/index.php
@@ -14,7 +14,7 @@ use scotthuangzl\googlechart\GoogleChart;
 //AppAsset::register($this);		/* INDEPENDENT CSS/JS/THEME FOR PAGE  Author: -ptr.nov-*/
 
 $this->sideCorp = 'PT. Lukisongroup';                                   /* Title Select Company pada header pasa sidemenu/menu samping kiri */
-$this->sideMenu = 'hrd_employee';                                       /* kd_menu untuk list menu pada sidemenu, get from table of database */
+$this->sideMenu = 'hrd_recrutmen';                                       /* kd_menu untuk list menu pada sidemenu, get from table of database */
 $this->title = Yii::t('app', 'HRM - Department Dashboard');             /* title pada header page */
 $this->params['breadcrumbs'][] = $this->title;                          /* belum di gunakan karena sudah ada list sidemenu, on plan next*/
 
